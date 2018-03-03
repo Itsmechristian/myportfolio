5b1afb4c807ea6c362200d045f200a44f71a25bf
SIjey2508 <vsijey2508@gmail.com>
2018-03-03 17:53:46 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+'use strict'
+  
+const express = require('express')
+    , exphbs = require('express-handlebars')
+    , path = require('path')
+    , app = express()
+
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'src')))
+
+app.get('/', (req, res) => {
+  res.render('body',
+   {
+    title: 'Welcome シ',
+    skills: [
+      {
+      title: 'Adobe XD',
+      percentWidth: 90 - 20,
+      percentage: 90
+      },
+      {
+      title: 'HTML',
+      percentWidth: 90 - 20,
+      percentage: 90
+      },
+      {
+      title: 'CSS',
+      percentWidth: 90 - 20,
+      percentage: 90
+      },
+      {
+      title: 'JavaScript',
+      percentWidth: 80 - 20,
+      percentage: 80
+      },
+      {
+      title: 'jQuery',
+      percentWidth: 80 - 20,
+      percentage: 80
+      },
+      {
+      title: 'React',
+      percentWidth: 70 - 20,
+      percentage: 70
+      },
+      {
+      title: 'Node.js',
+      percentWidth: 60 - 20,
+      percentage: 60
+      },
+      ]
+  })
+})
+
+const port = 8000;
+app.listen(port, console.log(`Connected to ${port}`))
